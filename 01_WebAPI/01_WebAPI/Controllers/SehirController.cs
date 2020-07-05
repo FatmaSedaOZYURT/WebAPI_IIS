b82f9040eef776b1f96c9913ea0be8a55b9ee8ef
FatmaSedaOZYURT <fsozyurt@hotmail.com>
2020-07-05 23:46:59 +0300

    Web API ISS projesi
    IIS mantığını anlamak adına yapmış olduğum bir projedir.

---
+++ 01_WebAPI/01_WebAPI/Controllers/SehirController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace _01_WebAPI.Controllers
+{
+    [EnableCors("*","*","*")]
+    public class SehirController : ApiController
+    {
+        //domainadresi/api/Sehir
+        string[] sehirler = new string[] { "Ankara", "İstanbul", "Karaman" };
+        public string[] Get()
+        {
+            return sehirler;
+        }
+        public string Get(int id)
+        {
+            return sehirler[id];
+        }
+    }
+}
---
+++ 01_WebAPI/01_WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_01_WebAPI.WebApiApplication" Language="C#" %>
